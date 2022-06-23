c6c933a2a93dc7b6e5a6e963495addc886b0d2bb
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-06-23 16:12:51 -0400

    added logical operators

--- Develop/script.js
+++ Develop/script.js
@@ -12,84 +12,100 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   let passLength = prompt("How long would you like your password to be (must be between 8-128 characters)?");
-  alert("OK" + " you want your password to be " + passLength + " characters long.");
-  console.log(passLength*("1"))
+  alert("OK" + " you want your password to be " + passLength + " characters long.")
+ 
+    if(passLength < 8 || passLength > 128){ 
+      alert("Your password is not in the appropriate range.")
+     }
+    
+     console.log(typeof passLength)
+
 
   let haveLowercase = prompt("Do you want your password to contain lowercase letters: yes/no?");
   alert("OK, you said " + haveLowercase + " to using lowercase letters in your password.");
-  console.log(haveLowercase)
+ 
+    if(haveLowercase !== "yes" && haveLowercase !== "no"){
+      alert("That does not meet the password criteria")
+    }
 
   let haveUppercase = prompt("Do you want your password to contain uppercase letters yes/no?");
   alert("OK, you said " + haveUppercase + " to using uppercase letters in your password.");
-  console.log(haveUppercase)
+
+  if(haveUppercase!== "yes" && haveUppercase !== "no"){
+    alert("That does not meet the password criteria")
+  }
 
   let haveNumbers = prompt("Do you want your password to contain numbers yes/no?");
   alert("OK, you said " + haveNumbers + " to using numbers in your password.");
-  console.log(haveNumbers)
+ 
+  if(haveNumbers !== "yes" && haveNumbers !== "no"){
+    alert("That does not meet the password criteria")
+  }
 
   let haveSpecial = prompt("Do you want your password to contain special characters yes/no");
   alert("OK, you said " + haveSpecial + " to using special characters in your password.");
-  console.log(haveSpecial)
-
+  
+  if(haveSpecial !== "yes" && haveSpecial !== "no"){
+    alert("That does not meet the password criteria")
+  }
 
 
   if (haveLowercase === "yes") {
-    pass = passHolder.lowercase;
-    console.log(pass);
+    passLC = passHolder.lowercase;
+  
 
   } else {
-    pass = "";
-    console.log(pass);
+    passLC = "";
+   
   }
 
 
   if (haveUppercase === "yes") {
-    blue = passHolder.uppercase;
-    console.log(blue);
+    passUC = passHolder.uppercase;
+    
 
   } else {
-    blue = "";
-    console.log(blue);
+    passUC = "";
+ 
   }
   
 
   if (haveNumbers === "yes") {
-    red = passHolder.numbers;
-    console.log(red)
+    passN = passHolder.numbers;
+   
 
   } else {
-    red = "";
-    console.log(red);
+    passN = "";
+   
   }
   
 
   if (haveSpecial === "yes") {
-    green = passHolder.specialC;
-    console.log(green);
+    passSC = passHolder.specialC;
+   
 
   } else{
-    green = "";
-    console.log(green);
+    passSC = "";
+    
   }
 
   
-  let brown = (green + red + blue + pass)
-  console.log(brown)
+  let passAll = (passLC + passUC + passN + passSC)
+  console.log(passAll)
+  
 
  
     var password = "";
 
     for (var i = 0; i < passLength; i++) {
       
-      password += brown.charAt(Math.floor(Math.random() *
-        passLength));
+      password += passAll.charAt(Math.floor(Math.random() *
+        passLength.length));
     }
    
-    console.log("hi")
- 
-    console.log(brown)
-    console.log(password)
+    
     alert("Your new password is: " + password);
+    
     return password
    
   }
